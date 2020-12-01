b7cbfc01ecbe18fca6dcce20516e697dc555683c
marvoqs <marvoqs@gmail.com>
2020-12-01 15:06:33 +0100

    create auth, games & users routes

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 
+// Define routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/games', require('./routes/api/games'));
+app.use('/api/users', require('./routes/api/users'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));

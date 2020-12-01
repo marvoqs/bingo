9bf39b1fd280e52c60b8b25e5de63641435f8be2
marvoqs <marvoqs@gmail.com>
2020-12-01 15:51:10 +0100

    add user registration

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// @route    GET api/users
+// @route    GET api/auth
 // @desc     Test route
 // @access   Private
 router.get('/', (req, res) => res.send('Auth route'));

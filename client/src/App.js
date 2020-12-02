5603225c30d5e9672f1039a8467a82cbd6b3a661
marvoqs <marvoqs@gmail.com>
2020-12-02 16:19:43 +0100

    add initial routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Game from './components/game/Game';
+import Admin from './components/admin/Admin';
 
 const App = () => {
-  return <h1>My app</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/:game_key' component={Game} />
+        <Route exact path='admin' component={Admin} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;

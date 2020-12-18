fb8b2c43744ebe5bccfcc02cb4cf44fecbaabc88
marvoqs <marvoqs@gmail.com>
2020-12-18 11:41:53 +0100

    prepare for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,14 +10,22 @@ connectDB();
 // Init middleware
 app.use(express.json({ extended: true }));
 
-app.get('/', (req, res) => res.send('API running'));
-
 // Define routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/games', require('./routes/api/games'));
 app.use('/api/tickets', require('./routes/api/tickets'));
 app.use('/api/users', require('./routes/api/users'));
 
-const PORT = process.env.PORT || 5000;
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
